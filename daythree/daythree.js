7d4af81b15552493748b1321484fc3e5183a959a
atharva-123 <atharvashirole1234@gmail.com>
2023-04-18 10:52:02 +0530

    First Commit

---
+++ daythree/daythree.js
@@ -0,0 +1,46 @@
+//single line comment
+/* multiline comment*/
+//  
+/*var b=2;
+ b*=5;
+ alert(b)
+
+ /*alert("WELCOME to my page")
+alert("Hello");
+alert(4+4);
+ let num=20;
+  alert(num);
+  const a=120;
+  alert(a);*/
+  // relational operator
+   /*var res=12<30;
+   alert(res);
+
+   // logical
+   alert(b>20 && b==60)
+
+   var x=10;
+   var y=20;
+   if (x<y) {
+     alert(true)
+   } else {
+    alert(false)
+    
+   }
+
+   
+   alert(typeof("atharva"));
+
+   let age=prompt("what is your age?")
+   alert(age);*/
+
+
+   function myFunction() {
+    var x = document.createElement("IMG");
+    x.setAttribute("src", "google.png");
+    x.setAttribute("width", "304");
+    x.setAttribute("height", "228");
+    x.setAttribute("alt", "The Pulpit Rock");
+    document.body.appendChild(x);
+  }
+  
